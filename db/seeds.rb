fd557bc2c39ee8ef66e1fb1769b138b2ae75ce45
damari <motokin128@gmail.com>
2021-05-28 12:15:49 +0900

    一覧画面を実装

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,9 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+email = "test@examole.com"
+password = "password"
+
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE posts RESTART IDENTITY CASCADE")
 
@@ -13,6 +16,12 @@ user1 = User.create!(email: "satou@example.com", password: "password")
 user2 = User.create!(email: "suzuki@example.com", password: "password")
 user3 = User.create!(email: "tanaka@example.com", password: "password")
 
-Post.create!(content: "おはよう", user_id: user2.id)
-Post.create!(content: "こんにちは", user_id: user3.id)
-Post.create!(content: "こんばんは", user_id: user3.id)
+user2.posts.create!(content: "沖縄そば")
+user1.posts.create!(content: "海ぶどう")
+user3.posts.create!(content: "ゴーヤーチャンプル")
+user3.posts.create!(content: "サーターアンダギー")
+user1.posts.create!(content: "タコライス")
+
+User.create!(email: email, password: password)
+
+puts "初期データの投入に成功しました！"

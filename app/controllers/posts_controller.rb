96c5a247cd4882fdb9babf2b91a31f3272a24bb4
damari <motokin128@gmail.com>
2021-05-28 15:25:42 +0900

    新規投稿機能と詳細画面を実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,16 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new
   end
 
   def create
+    post = current_user.posts.create!(post_params)
+    redirect_to post
   end
 
   def show
+    @post = Post.find(params[:id]) 
   end
 
   def edit
@@ -19,5 +23,11 @@ class PostsController < ApplicationController
   end
 
   def destroy
+  end
+
+  private
+
+  def post_params
+    params.require(:post).permit(:content)
   end
 end

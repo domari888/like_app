95e0d9357021ffead16a277b6628b1676acf29f2
damari <motokin128@gmail.com>
2021-05-29 15:20:25 +0900

    初期データを修正し、中間テーブルを設定

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,17 +10,24 @@ email = "test@examole.com"
 password = "password"
 
 ActiveRecord::Base.connection.execute("TRUNCATE TABLE users RESTART IDENTITY CASCADE")
-ActiveRecord::Base.connection.execute("TRUNCATE TABLE posts RESTART IDENTITY CASCADE")
 
 user1 = User.create!(email: "satou@example.com", password: "password")
 user2 = User.create!(email: "suzuki@example.com", password: "password")
 user3 = User.create!(email: "tanaka@example.com", password: "password")
 
-user2.posts.create!(content: "沖縄そば")
-user1.posts.create!(content: "海ぶどう")
-user3.posts.create!(content: "ゴーヤーチャンプル")
-user3.posts.create!(content: "サーターアンダギー")
-user1.posts.create!(content: "タコライス")
+post1 = user2.posts.create!(content: "沖縄そば")
+post2 = user1.posts.create!(content: "海ぶどう")
+post3 = user3.posts.create!(content: "ゴーヤーチャンプル")
+post4 = user3.posts.create!(content: "サーターアンダギー")
+post5 = user1.posts.create!(content: "タコライス")
+
+post1.likes.create!(user_id: user1.id)
+post1.likes.create!(user_id: user3.id)
+post2.likes.create!(user_id: user2.id)
+post3.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user1.id)
+post4.likes.create!(user_id: user2.id)
+post5.likes.create!(user_id: user3.id)
 
 User.create!(email: email, password: password)
 
